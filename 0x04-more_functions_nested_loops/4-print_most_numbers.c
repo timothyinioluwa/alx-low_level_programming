52163994ef31de411757a335e08e5b43583bf7a2
Timothy Ajani <101853460+timothyinioluwa@users.noreply.github.com>
2022-10-20 15:41:41 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,23 +1,19 @@
 #include "main.h"
-
 /**
- * print_most_numbers - function that prints the numbers from 0 to 9
- * do not print 2 and 4
- * can only use _putchar twice in the code
- * Return: print_most_numbers
- */
-
+*print_most_numbers - prints numbers except 2 and 4
+*
+*Return: returns nothing
+*/
 void print_most_numbers(void)
 {
-	int i = '0';
-
-	while (i <= '9')
-	{
-		if (i != '2' && i != '4')
-		{
-			_putchar(i);
-			i++;
-		}
-		_putchar('\n');
-	}
+int number;
+for (number = 48; number < 58; number++)
+{
+if ((number == 50) || (number == 52))
+{
+continue;
+}
+_putchar(number);
+}
+_putchar(10);
 }
